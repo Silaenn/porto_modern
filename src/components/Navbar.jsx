15fab1f08081cbdc577b8e36a53b677e79a5e86f
Deo silaen <deokeldisilaen@gmail.com>
2024-09-04 11:57:27 +0700

    push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,14 +77,14 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } p-6 bg-[#323232] absolute top-14 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
             <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
                   className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                    active === nav.title ? "text-white" : "text-secondary"
+                    active === nav.title ? "text-white" : "text-[#E0E0E0]"
                   }`}
                   onClick={() => {
                     setToggle(!toggle);
